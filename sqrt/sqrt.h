155fe9df003ba31f1be2772a0dba54da23619c4b
Oliver Leighton <oleighton@ucla.edu>
2017-07-30 19:30:36 -0700

    Format update
    Making order of header consistent with order of implementation file.

--- sqrt/sqrt.h
+++ sqrt/sqrt.h
@@ -1,9 +1,9 @@
 #ifndef SQRT
 #define SQRT
 
-double sqrt(int num);
+int truncate(int num, int digits);
 int factorial(int num);
 int digits(int num);
-int truncate (int num, int digits);
+double sqrt(int num);
 
 #endif
